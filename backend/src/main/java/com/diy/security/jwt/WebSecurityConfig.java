a2660a2217b7d9fc255dde48f289f2830a93bdca
Alex <lexcrd@yahoo.com>
2020-12-26 11:24:46 +0200

    Fix CORS

--- backend/src/main/java/com/diy/security/jwt/WebSecurityConfig.java
+++ backend/src/main/java/com/diy/security/jwt/WebSecurityConfig.java
@@ -51,6 +51,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable()
+                .cors().and()
                 .authorizeRequests().antMatchers("/authenticate", "/register").permitAll()
                 .anyRequest().authenticated().and()
                 .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and()
